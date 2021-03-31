09837ae86d3d62cab3790811ae9e251782f8f0d8
yondav <yonidavid.abr@gmail.com>
2021-03-31 00:42:29 -0400

    clean up generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -139,8 +139,6 @@ function renderLicenseLink(license) {
   }
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
 function renderLicenseSection(license) {
   const licenseSection = `## License
   
@@ -149,13 +147,6 @@ function renderLicenseSection(license) {
   renderLicenseBadge(license);
 }
 
-// TODO: Create a function to generate markdown for README
-// function generateMarkdown() {
-//   // return `# ${data.title}
-//   // `;
-// }
-
-// module.exports = generateMarkdown;
 module.exports = {
   renderLicenseBadge,
   renderLicenseLink,
