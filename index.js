6428b6f764046d5773ae479aac346b60a5bb81d0
yondav <yonidavid.abr@gmail.com>
2021-03-31 03:51:07 -0400

    app works. readme looks solid

--- index.js
+++ index.js
@@ -17,29 +17,81 @@ const questions = [
     message: 'Project Title',
   },
   {
+    name: 'deployed',
+    type: 'input',
+    message: 'Deployed Link',
+  },
+  {
+    name: 'repo',
+    type: 'input',
+    message: 'Repository Name',
+  },
+  {
+    name: 'repoLink',
+    type: 'input',
+    message: 'Repository Link',
+  },
+  {
     name: 'description',
     type: 'input',
-    message: 'Project Description',
+    message:
+      'Project Description -- Supports code-blocks. format: ```code```, -r to return, * for bullet points',
   },
   {
     name: 'install',
     type: 'input',
-    message: 'Installation Guidelines',
+    message:
+      'Installation Guidelines -- Supports code-blocks. format: ```code```, -r to return, * for bullet points',
   },
   {
     name: 'usage',
     type: 'input',
-    message: 'Usage Information',
-  },
-  {
-    name: 'contribution',
-    type: 'input',
-    message: 'Contribution Guidelines',
+    message:
+      'Usage Information -- Supports code-blocks. format: ```code```, -r to return, * for bullet points',
   },
   {
     name: 'test',
     type: 'input',
-    message: 'Test Instructions',
+    message:
+      'Test Instructions -- Supports code-blocks. format: ```code```, -r to return, * for bullet points',
+  },
+  {
+    name: 'author',
+    type: 'input',
+    message: 'Author Name',
+  },
+  {
+    name: 'github',
+    type: 'input',
+    message: 'Github Link',
+  },
+  {
+    name: 'email',
+    type: 'input',
+    message: 'Email Address',
+  },
+  {
+    name: 'contact',
+    type: 'input',
+    message: 'Contact Instructions -- -r to return, * for bullet points',
+  },
+  {
+    name: 'contribution',
+    type: 'input',
+    message:
+      'Contribution Guidelines -- Supports code-blocks. format: ```code```, -r to return, * for bullet points',
+  },
+  {
+    name: 'reportIssues',
+    type: 'input',
+    message:
+      'Reporting Issues -- Supports code-blocks. format: ```code```, -r to return, * for bullet points',
+  },
+  {
+    name: 'knownIssues',
+    type: 'input',
+    message:
+      'Known Issues -- Supports code-blocks. format: ```code```, -r to return, * for bullet points',
   },
   {
     name: 'license',
@@ -61,49 +113,103 @@ const questions = [
       'The zlib/libpng License',
     ],
   },
-  {
-    name: 'github',
-    type: 'input',
-    message: 'Github Username',
-  },
-  {
-    name: 'email',
-    type: 'input',
-    message: 'Email Address',
-  },
-  {
-    name: 'contact',
-    type: 'input',
-    message: 'Contact Instructions',
-  },
 ];
 
 const renderMD = ({
-  licenseLinks,
-  licenseBadges,
   project,
+  deployed,
+  repo,
+  repoLink,
   description,
   install,
   usage,
-  contribution,
   test,
-  license,
+  author,
   github,
   email,
   contact,
+  contribution,
+  reportIssues,
+  knownIssues,
+  license,
+  licenseLinks,
+  licenseBadges,
 }) => {
-  return `${project}
-  ${licenseBadges}
-  ${licenseLinks}
-  ${description.replace(regexCode, ticks).replace(regexReturn, responseReturn)}
-  ${install.replace(regexCode, ticks).replace(regexReturn, responseReturn)}
-  ${usage.replace(regexCode, ticks).replace(regexReturn, responseReturn)}
-  ${contribution.replace(regexCode, ticks).replace(regexReturn, responseReturn)}
-  ${test.replace(regexCode, ticks).replace(regexReturn, responseReturn)}
-  ${license}
-  ${github.replace(regexReturn, responseReturn)}
-  ${email.replace(regexReturn, responseReturn)}
-  ${contact.replace(regexReturn, responseReturn)}`;
+  return `
+# ${project} / ${repo}
+
+* <a href="${deployed}">${project}</a>
+* <a href="${repoLink}">${repo}</a>
+
+${licenseBadges}
+
+## About / Synopsis
+
+${description.replace(regexCode, ticks).replace(regexReturn, responseReturn)}
+
+## Table of contents
+
+> * [Title / Repository Name](#title--repository-name)
+>   * [About / Synopsis](#about--synopsis)
+>   * [Table of contents](#table-of-contents)
+>   * [Installation](#installation)
+>   * [Usage](#usage)
+>   * [Test instructions](#test-instructions)
+>   * [Author](#author)
+>     * [Contact](#contact)
+>   * [Contributing / Issues](#contributing--issues)
+>     * [Contributing](#contributing)
+>     * [Known Issues](#known-issues)
+>     * [Reporting Issues](#reporting-issues)
+${license === [] ? '' : '>   * [License](#license)'}
+
+## Installation
+
+${install.replace(regexCode, ticks).replace(regexReturn, responseReturn)}
+
+## Usage
+
+${usage.replace(regexCode, ticks).replace(regexReturn, responseReturn)}
+
+## Test Instructions
+
+${test.replace(regexCode, ticks).replace(regexReturn, responseReturn)}
+
+## Author
+
+* ${author}
+* <a href="${github}
+">Github</a>
+* <a href="mailto:${email}">${email}</a>
+
+---
+
+### Contact
+
+${contact.replace(regexReturn, responseReturn)}
+
+## Contributing / Issues
+
+---
+
+### Contributing
+
+${contribution.replace(regexCode, ticks).replace(regexReturn, responseReturn)}
+
+---
+
+### Known Issues
+
+${knownIssues.replace(regexCode, ticks).replace(regexReturn, responseReturn)}
+
+---
+
+### Reporting Issues
+
+${reportIssues.replace(regexCode, ticks).replace(regexReturn, responseReturn)}
+
+${license === [] ? '' : '## License\n\n' + licenseLinks}
+`;
 };
 
 // TODO: Create a function to initialize app
